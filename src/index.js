f477fa6b1e1c4b0a97c17899151d3154d7d8d4f4
jacob kleiman <jacobakleiman@gmail.com>
2020-07-20 14:01:29 -0700

    adds color assets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { ThemeProvider } from 'styled-components';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/utils/colors.js
@@ -0,0 +1,57 @@
+export const blue = {
+    100: "#3a36e0",
+    200: "#0804b8",
+    300: "#030086",
+    400: "#5f25a4",
+    500: "#050449"
+  };
+
+  export const purple = {
+      100: "#9DA0BD",
+      150: "#98A1F0",
+      200: "#7280FF",
+      300: "#515CBD",
+      400: "#202B8A",
+      500: "#23315E"
+  }
+  
+  export const blond = {
+    100: "#EBEADF",
+    200: "#FEF5CA",
+    300: "#F0E798",
+    400: "#FFDBA9"
+  }
+
+  export const hotPink = {
+      100: "#C8B0BB",
+      200: "#FF91C2",
+      300: "#FF6CAE",
+      400: "#F15C9F"
+  }
+
+  export const green = {
+    100: "#529e66",
+    200: "#367b48",
+    300: "#276738"
+  };
+  
+  export const yellow = {
+    100: "#e1c542",
+    200: "#cab23f",
+    300: "#b49e35"
+  };
+  
+  export const red = {
+    100: "#d0454c",
+    200: "#b54248",
+    300: "#95353a"
+  };
+  
+  export const neutral = {
+    100: "#ffffff",
+    200: "#f4f5f7",
+    300: "#e1e1e1",
+    400: "#737581",
+    500: "#4a4b53",
+    600: "#000000"
+  };
