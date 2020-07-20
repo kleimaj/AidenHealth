61c61ade2a84cc58df5d0bc229b0df273b8e1eab
jacob kleiman <jacobakleiman@gmail.com>
2020-07-20 14:34:24 -0700

    adds typography assets, global styles, css reset, and base font of 16px

---
+++ src/utils/typography.js
@@ -0,0 +1,12 @@
+export const primaryFont = '"Work Sans", sans-serif';
+
+export const typeScale = {
+    header1: "3.0519rem",
+    header2: "2.4413rem",
+    header3: "1.9531rem",
+    header4: "1.5625",
+    header5: "1.25rem",
+    paragraph: "1rem",
+    helperText: "0.8rem",
+    copyrightText: "0.7rem"
+}
