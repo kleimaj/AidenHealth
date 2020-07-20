9f76826fe8800670cea060eda1b3645ba0379e1a
jacob kleiman <jacobakleiman@gmail.com>
2020-07-20 15:28:11 -0700

    adds useMediaQuery hook for inline styling

---
+++ src/hooks/hooks.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from 'react';
+
+// useMediaQuery hook
+// by Medium user Tim Tennant
+// https://medium.com/@ttennant/react-inline-styles-and-media-queries-using-a-custom-react-hook-e76fa9ec89f6
+export const useMediaQuery = (query) => {
+  const mediaMatch = window.matchMedia(query);
+  const [matches, setMatches] = useState(mediaMatch.matches);
+
+  useEffect(() => {
+    const handler = e => setMatches(e.matches);
+    mediaMatch.addListener(handler);
+    return () => mediaMatch.removeListener(handler);
+  });
+  return matches;
+};
