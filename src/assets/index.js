989762a7339475839eebb0fe6fbcce0938ee0b0f
jacob kleiman <jacobakleiman@gmail.com>
2020-07-22 10:10:38 -0700

    configures badge exports

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,7 @@ import HeroImage from './illustrations/hero-image.svg'
 
 export * from './icons/close-icon';
 export * from './icons/hamburger-icon';
+export * from './icons/badges';
 
 export const Illustrations = {
     SignUp,
