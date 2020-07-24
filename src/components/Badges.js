348c9592e54f9e7ddb560a9de05145aad1427796
jacob kleiman <jacobakleiman@gmail.com>
2020-07-24 14:07:18 -0700

    edits badges

--- src/components/Badges.js
+++ src/components/Badges.js
@@ -5,10 +5,12 @@ import { PlusIcon, PercentIcon, CheckIcon } from '../assets/';
 
 const BadgesWrapper = styled.div`
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    justify-content: space-around;
     
     @media only screen and (max-width: 1024px) {
         margin: 0 auto;
+        flex-direction: column;
     }
 `;
 
@@ -18,14 +20,15 @@ const Badge = styled.div`
     justify-content: start;
     // padding: 50px;
     padding: 20px 0;
-    transition: all 0.2s linear;
+    // transition: all 0.2s linear;
+    flex-direction: column;
     svg {
         width: 65px;
     }
-    &:hover {
-        transition: all 0.2s linear;
-        transform: translateY(-10px);
-    }
+    // &:hover {
+    //     transition: all 0.2s linear;
+    //     transform: translateY(-10px);
+    // }
     // Mobile
     @media only screen and (max-width: 1024px) {
         padding: 25px;
@@ -40,13 +43,15 @@ const Badge = styled.div`
     }
     // Large Monitor
     @media only screen and (min-width: 1600px) { 
-        padding: 0 300px;
+        // padding: 0 300px;
+        padding: 0 50px;
     }
 `;
 
 const Title = styled.h3`
     font-size: ${typeScale.header4};
-    padding: 30px;
+    // padding: 30px;
+    padding: 0 30px;
     text-align: left;
     @media only screen and (max-width: 1024px) {
         padding: 0;
