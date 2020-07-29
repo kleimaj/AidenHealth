d9ec0dc597302b518302266ccb7ddb5efa75d2b1
jacob kleiman <jacobakleiman@gmail.com>
2020-07-29 12:46:20 -0700

    Updates

---
+++ precache-manifest.5741c3ea93d045691446970aa7fcf1fc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c8847a4c46fe5a031384dc72e0c611",
+    "url": "/AidenHealth/index.html"
+  },
+  {
+    "revision": "c416e25334461ad8ac3e",
+    "url": "/AidenHealth/static/css/main.8e964da6.chunk.css"
+  },
+  {
+    "revision": "d603d2eb8dd47c9ed6d9",
+    "url": "/AidenHealth/static/js/2.06a264b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AidenHealth/static/js/2.06a264b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c416e25334461ad8ac3e",
+    "url": "/AidenHealth/static/js/main.465ee282.chunk.js"
+  },
+  {
+    "revision": "8905e9cff23ee8c2c5b8",
+    "url": "/AidenHealth/static/js/runtime-main.9db292e9.js"
+  },
+  {
+    "revision": "b98ca65ccfdc9e032b32b545a49e7860",
+    "url": "/AidenHealth/static/media/apply.b98ca65c.svg"
+  },
+  {
+    "revision": "f87d439c43a11c714a16255f0848608f",
+    "url": "/AidenHealth/static/media/help.f87d439c.svg"
+  },
+  {
+    "revision": "bf406a95be2d19807b114d25e4e9c4c8",
+    "url": "/AidenHealth/static/media/hero-image.bf406a95.svg"
+  },
+  {
+    "revision": "ae9463265b498c56de406fe1ff724b5e",
+    "url": "/AidenHealth/static/media/hospital.ae946326.svg"
+  },
+  {
+    "revision": "0ddb8d3f68d7d04e3286a77415d89829",
+    "url": "/AidenHealth/static/media/pay.0ddb8d3f.svg"
+  },
+  {
+    "revision": "40592418d605ca6d3beed1583e86a8ec",
+    "url": "/AidenHealth/static/media/shop.40592418.svg"
+  },
+  {
+    "revision": "0c5d33b71cb5a4fef14e63b46e36c040",
+    "url": "/AidenHealth/static/media/sign-up.0c5d33b7.svg"
+  },
+  {
+    "revision": "dea03ebb382f922292e0637266144fdb",
+    "url": "/AidenHealth/static/media/success.dea03ebb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dbbc8c8f22f93b0c83152760a524339.js"
+  "/AidenHealth/precache-manifest.5741c3ea93d045691446970aa7fcf1fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AidenHealth/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
