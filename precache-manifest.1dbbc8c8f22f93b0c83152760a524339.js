0e53f618ff74b6ba3bd491a965f19ac4b5e975a1
jacob kleiman <jacobakleiman@gmail.com>
2020-07-29 12:32:00 -0700

    Updates

---
+++ precache-manifest.1dbbc8c8f22f93b0c83152760a524339.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a3aef9cd236013cf620aa0bf79075d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c416e25334461ad8ac3e",
+    "url": "/static/css/main.8e964da6.chunk.css"
+  },
+  {
+    "revision": "3f0d64b9e508b1f29808",
+    "url": "/static/js/2.a9ee0626.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a9ee0626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c416e25334461ad8ac3e",
+    "url": "/static/js/main.465ee282.chunk.js"
+  },
+  {
+    "revision": "6fa87921d793f97e51f9",
+    "url": "/static/js/runtime-main.d50eee57.js"
+  },
+  {
+    "revision": "b98ca65ccfdc9e032b32b545a49e7860",
+    "url": "/static/media/apply.b98ca65c.svg"
+  },
+  {
+    "revision": "f87d439c43a11c714a16255f0848608f",
+    "url": "/static/media/help.f87d439c.svg"
+  },
+  {
+    "revision": "bf406a95be2d19807b114d25e4e9c4c8",
+    "url": "/static/media/hero-image.bf406a95.svg"
+  },
+  {
+    "revision": "ae9463265b498c56de406fe1ff724b5e",
+    "url": "/static/media/hospital.ae946326.svg"
+  },
+  {
+    "revision": "0ddb8d3f68d7d04e3286a77415d89829",
+    "url": "/static/media/pay.0ddb8d3f.svg"
+  },
+  {
+    "revision": "40592418d605ca6d3beed1583e86a8ec",
+    "url": "/static/media/shop.40592418.svg"
+  },
+  {
+    "revision": "0c5d33b71cb5a4fef14e63b46e36c040",
+    "url": "/static/media/sign-up.0c5d33b7.svg"
+  },
+  {
+    "revision": "dea03ebb382f922292e0637266144fdb",
+    "url": "/static/media/success.dea03ebb.svg"
+  }
+]);
