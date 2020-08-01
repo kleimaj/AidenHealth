edfc81cc4b73bfbb72ad96a0acee60362baf018e
jacob kleiman <jacobakleiman@gmail.com>
2020-08-01 14:24:32 -0700

    initializes blockquote

---
+++ src/components/Blockquote.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { typeScale } from "../utils";
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`
+
+export const Blockquote = () => {
+    return (
+        <Wrapper>
+            
+        </Wrapper>
+    )
+}
