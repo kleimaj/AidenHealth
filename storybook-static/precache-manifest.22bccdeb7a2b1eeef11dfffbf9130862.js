90500420f7dcfbddf35766c348e14fe9a960384b
jacob kleiman <jacobakleiman@gmail.com>
2020-08-02 23:58:41 -0700

    builds storybook

---
+++ storybook-static/precache-manifest.22bccdeb7a2b1eeef11dfffbf9130862.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648b0f44ead647560546275abf5f3572",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.6d21c81fa3df04861f3d.bundle.js"
+  },
+  {
+    "url": "runtime~main.6d21c81fa3df04861f3d.bundle.js"
+  },
+  {
+    "revision": "34ed65cf4a07b238ce56",
+    "url": "static/css/main.8e964da6.chunk.css"
+  },
+  {
+    "revision": "b98ca65ccfdc9e032b32b545a49e7860",
+    "url": "static/media/apply.b98ca65c.svg"
+  },
+  {
+    "revision": "2933420f80d5d036fde367ddc5d07695",
+    "url": "static/media/color1.2933420f.png"
+  },
+  {
+    "revision": "d08cf8f0d5334553117084e2ed222805",
+    "url": "static/media/color2.d08cf8f0.png"
+  },
+  {
+    "revision": "f87d439c43a11c714a16255f0848608f",
+    "url": "static/media/help.f87d439c.svg"
+  },
+  {
+    "revision": "c421ab315ee98447708004e2c04ce434",
+    "url": "static/media/hero-image.c421ab31.svg"
+  },
+  {
+    "revision": "ad4b24a723110fb525a166e1ca66730e",
+    "url": "static/media/hospital.ad4b24a7.svg"
+  },
+  {
+    "revision": "0ddb8d3f68d7d04e3286a77415d89829",
+    "url": "static/media/pay.0ddb8d3f.svg"
+  },
+  {
+    "revision": "3bff383f925bb55428486d91d43a4fbb",
+    "url": "static/media/persona1.3bff383f.svg"
+  },
+  {
+    "revision": "bfaa4f572837563fb40e22d0785f0591",
+    "url": "static/media/shop.bfaa4f57.svg"
+  },
+  {
+    "revision": "97c7134b8ba21151fef9ae40af0aedad",
+    "url": "static/media/sign-in.97c7134b.svg"
+  },
+  {
+    "revision": "84a2efc2fa528d105afa9e993db1fce1",
+    "url": "static/media/sign-up.84a2efc2.svg"
+  },
+  {
+    "revision": "dea03ebb382f922292e0637266144fdb",
+    "url": "static/media/success.dea03ebb.svg"
+  },
+  {
+    "url": "vendors~main.6d21c81fa3df04861f3d.bundle.js"
+  },
+  {
+    "url": "vendors~main.6d21c81fa3df04861f3d.bundle.js.LICENSE.txt"
+  }
+]);
